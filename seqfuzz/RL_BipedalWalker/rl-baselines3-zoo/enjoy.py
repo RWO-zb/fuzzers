c3b25cf2f734b273fba6e103c6c7becdba58b24f
“RWO-zb” <13762659881@163.com>
2025-12-07 21:31:17 +0800

    modify

--- seqfuzz/RL_BipedalWalker/rl-baselines3-zoo/enjoy.py
+++ seqfuzz/RL_BipedalWalker/rl-baselines3-zoo/enjoy.py
@@ -201,7 +201,7 @@ def main():
     ep_len = 0
     successes = []
     fuzzer = fuzzing()
-    seeds_num = 100
+    seeds_num = 500
     i = 0
     pbar = tqdm.tqdm(total=seeds_num)
     while i < seeds_num:
@@ -273,7 +273,7 @@ def main():
     noCrashF_40 = open(os.path.join(result_path, 'noCrashStateSeqV2_40.txt'), mode='a')
     timeStamp = open(os.path.join(result_path, 'timeStamp.txt'), mode='a')
     seedcount = 0
-    while current_time - start_fuzz_time < 3600 * 12 and len(fuzzer.corpus) > 0 and seedcount<100:
+    while current_time - start_fuzz_time < 3600 * 12 and len(fuzzer.corpus) > 0 :
     # while current_time - start_fuzz_time < 3600 * 12 and len(fuzzer.corpus) > 0:
         is_crash = False
         seedcount+=1
